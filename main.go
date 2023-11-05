e6294832800dcf30e1887e845a00c35e7e26a3ae
zarazan <zarazan@users.noreply.github.com>
2023-11-04 22:08:12 -0600

    wip

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ojo-network/indexer/indexer"
 	"github.com/rs/zerolog"
@@ -10,9 +12,15 @@ import (
 
 func main() {
 
+	// Listen for user interrupt
+	userInterrupt := make(chan os.Signal, 1)
+	signal.Notify(userInterrupt, os.Interrupt, syscall.SIGTERM)
+
+	// Create logger
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+
+	// Create context
 	ctx := context.Background()
 
 	indexer := indexer.NewIndexer(logger, ctx)
-
 }
