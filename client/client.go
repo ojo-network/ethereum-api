04f98b93cf97d0f7eec5c2a9dec9e05cd6dd3fe2
Kyle Zarazan <zarazan@users.noreply.github.com>
2023-11-05 13:52:26 -0700

    prototype works

--- client/client.go
+++ client/client.go
@@ -1,12 +1,17 @@
 package client
 
 import (
-	"log"
+	"context"
+	"fmt"
+
+	sdkmath "cosmossdk.io/math"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ojo-network/ethereum-api/abi"
+	"github.com/ojo-network/indexer/indexer"
+	"github.com/ojo-network/indexer/utils"
 )
 
 const (
@@ -14,29 +19,55 @@ const (
 	poolAddress = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
 )
 
-func Test() {
-
+func WatchSwapEvent(i *indexer.Indexer, ctx context.Context) error {
 	ethClient, err := ethclient.Dial(nodeUrl)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	poolFilterer, err := abi.NewPoolFilterer(common.HexToAddress(poolAddress), ethClient)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	eventSink := make(chan *abi.PoolSwap)
 
 	opts := &bind.WatchOpts{Start: nil, Context: ctx}
 	subscription, err := poolFilterer.WatchSwap(opts, eventSink, nil, nil)
-
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	subscriptionErr := subscription.Err()
+	srvErrCh := make(chan error, 1)
 
-	subscription.Unsubscribe()
+	go func() {
+		select {
+		case <-ctx.Done():
+			subscription.Unsubscribe()
+			srvErrCh <- nil
+			break
+		case err := <-subscription.Err():
+			srvErrCh <- err
+			break
+		case event := <-eventSink:
+			swap := indexer.Swap{
+				BlockNum:     indexer.BlockNum(event.Raw.BlockNumber),
+				Timestamp:    utils.CurrentUnixTime(),
+				ExchangePair: "WETH/ETH",
+				AmountIn:     sdkmath.LegacyNewDecFromBigInt(event.Amount0),
+				AmountOut:    sdkmath.LegacyNewDecFromBigInt(event.Amount1),
+			}
+			i.AddSwap(swap)
+			fmt.Println(event)
+		}
+	}()
 
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case err = <-srvErrCh:
+			return err
+		}
+	}
 }
--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 	"os/signal"
 	"syscall"
 
+	"golang.org/x/sync/errgroup"
+
+	"github.com/ojo-network/ethereum-api/client"
 	"github.com/ojo-network/indexer/indexer"
+	"github.com/ojo-network/indexer/server"
 	"github.com/rs/zerolog"
 )
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+}
 
 func main() {
 
@@ -20,7 +28,51 @@ func main() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
 
 	// Create context
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 
-	indexer := indexer.NewIndexer(logger, ctx)
+	// Cancel the context on user interrupt
+	go func() {
+		<-userInterrupt
+		logger.Info().Msg("user interrupt")
+		cancel()
+	}()
+
+	// Initialize indexer
+	i := indexer.NewIndexer(logger, ctx)
+
+	// Start websocket server and broadcast prices
+	cfg := server.ServerConfig{
+		ListenAddr:        "0.0.0.0:5005",
+		WriteTimeout:      "20s",
+		ReadTimeout:       "20s",
+		BroadcastInterval: "5s",
+	}
+	s, err := server.NewServer(logger, cfg)
+	if err != nil {
+		logger.Error().Err(err).Msg("error creating server")
+		cancel()
+	}
+
+	g, _ := errgroup.WithContext(ctx)
+
+	// begin the websocket server process
+	g.Go(func() error {
+		defer cancel()
+		err := s.StartWebsocketAPI(ctx, logger, i)
+		logger.Info().Msg("websocket server exited")
+		return err
+	})
+
+	// begin the websocket server process
+	g.Go(func() error {
+		defer cancel()
+		err := client.WatchSwapEvent(i, ctx)
+		logger.Info().Msg("ethereum client exited")
+		return err
+	})
+
+	err = g.Wait()
+	if err != nil {
+		logger.Error().Err(err).Send()
+	}
 }
